6488666bdfdfb391f5c1ea6d09dcfdc880c3a0e8
oluwapelumi temiloluwa <temiloluwaayomide52@gmail.com>
2025-06-30 08:00:32 +0100

    first commit

---
+++ Work 2.py
@@ -0,0 +1,32 @@
+# firstname = input ("Enter your first name?: ")
+# lastname= input("Enter your last name?: ")
+# Firstname= firstname.lower()
+# Lastname= lastname.lower()
+# print(f"{Firstname} {Lastname}")
+# FIRSTNAME= firstname.upper()
+# LASTNAME = lastname.upper()
+# print(f"{FIRSTNAME} {LASTNAME}")
+
+# price = float(input("Enter the price:? "))
+# quantity= int(input("Enter the quanitity?: "))
+# totalcost = price*quantity
+# if totalcost > 100:
+#     total= totalcost* 0.9
+# print(f"your total amount is {totalcost}")
+
+# name= input("Enter your name? ")
+# Name = name.upper()
+# print(Name)
+
+# num1 = int(input("Enter a number?: "))
+# num2= int(input("Enter a number?: "))
+# Add = num1+num2
+# sub = num1-num2
+# mult = num1*num2
+# divi = num1/num2
+# print(Add)
+# print(sub)
+# print(mult)
+# print(divi)
+
+
